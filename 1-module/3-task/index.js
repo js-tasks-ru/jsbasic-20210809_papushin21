d8665db173ddd3a31459ba3fb58ab52d66b3fed1
Michael Papushin <papushin21@gmail.com>
2021-08-25 17:52:27 +0300

    Добавил пару пробелов.

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,10 +3,12 @@ function ucFirst(str) {
   if (str == '') {
     return '';
   }
+  
   let otherPart = str.slice(1);
   
   str = str[0].toUpperCase();
 
   return str + otherPart;
+
 }
 ucFirst('');
