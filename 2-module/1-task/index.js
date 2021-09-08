e2d6876aaf3428c10fdf30774f170d2202fae167
Michael Papushin <papushin21@gmail.com>
2021-09-08 18:49:52 +0300

    Написал функцию, которая высчитывает зп сотрудников. Сначала идет счетчик суммы зп. После этого при помощи for..in перебираем ключи - свойства в объекте. Они проходят проверку на сравнение с типом число. Если значение ключа - число, то isSalary становится true. В цикле if(isSalary) при true происходит вычисление счетчика суммы с числовыми значениями ключей. Далее - возвращение суммы числовых значений. Не сделал проверку на специальные значения - при внесении условия if (isSalary !== NaN) код ломается.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,19 @@
 function sumSalary(salaries) {
-  // ваш код...
+  
+  let sumOfSalaries = 0;
+
+  for (let key in salaries) {
+
+
+    let isSalary = typeof salaries[key] === "number";
+
+    
+    if (isSalary) {
+
+      sumOfSalaries += salaries[key];
+    
+    }
+
+  }
+  return sumOfSalaries;
 }
